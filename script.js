1bedcce485b8c51972fa8d0909c286892993b7f9
stefangoolcharan <goolcharanshivan@gmail.com>
2023-07-27 16:27:35 -0700

    desktop responsivness

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ menu.onclick = () => {
     navbar.classList.toggle('open');
 }
 
+const fadeInDiv = document.querySelector('.fade-in');
+const fadeInDiv2 = document.querySelector('.see-more')
+
+const fadeInOptions = {
+  threshold: 0.3 
+};
+
+const fadeInObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = '1';
+      fadeInObserver.unobserve(entry.target);
+    }
+  });
+}, fadeInOptions);
+
+fadeInObserver.observe(fadeInDiv);
+
+const fadeInObserver2 = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = '1';
+        fadeInObserver.unobserve(entry.target);
+      }
+    });
+  }, fadeInOptions);
+  
+  fadeInObserver.observe(fadeInDiv2);
